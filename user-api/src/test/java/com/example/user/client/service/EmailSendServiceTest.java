529e2dc14b7588501f80562e0deba8bd986110ce
0jo-gil <dudwh921112@naver.com>
2023-04-02 23:22:04 +0900

    feat : [001-EMAIL-Connect] mailgun에 대한 email 연동
    무료계정 테스트 시 이메일 등록 필요

--- user-api/src/test/java/com/example/user/client/service/EmailSendServiceTest.java
+++ user-api/src/test/java/com/example/user/client/service/EmailSendServiceTest.java
@@ -1,18 +1,27 @@
 package com.example.user.client.service;
-
-import com.example.user.service.test.EmailSendService;
+import com.example.user.client.MailgunClient;
+import com.example.user.client.mailgun.SendMailForm;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.util.Assert;
 
 @SpringBootTest
 class EmailSendServiceTest {
 
     @Autowired
-    private EmailSendService emailSendService;
+    private MailgunClient mailgunClient;
     @Test
     public void sendEmail() {
-        String response = emailSendService.sendEmail();
-        System.out.println(response);
+        SendMailForm form = SendMailForm.builder()
+                .from("dudwh921112@naver.com")
+                .to("dudwh921112@naver.com")
+                .subject("Mailgun test")
+                .text("test")
+                .build();
+
+        String response = mailgunClient.sendEmail(form).getBody();
+
+        Assert.isTrue(response != null);
     }
 }
